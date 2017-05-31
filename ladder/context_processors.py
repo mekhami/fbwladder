51e210ba2c4f080d90d7fd3564bd6506f0df53a4
Lawrence Vanderpool <lawrence.vanderpool@lab7.io>
2017-05-30 20:57:02 -0500

    add notification with link to admin panel for otustanding reports

--- ladder/context_processors.py
+++ ladder/context_processors.py
@@ -3,3 +3,9 @@ def unconfirmed_matches(request):
         return {'unconfirmed_matches': request.user.unconfirmed_matches}
     else:
         return {}
+
+def outstanding_reports(request):
+    if request.user.is_authenticated() and request.user.is_superuser:
+        return {'outstanding_reports': Report.objects.filter(resolved=False).count()}
+    else:
+        return {}
