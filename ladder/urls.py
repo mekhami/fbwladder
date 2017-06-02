8cbb251446a8db4a2ca3c16f00032495474cb38d
Lawrence <lawrence.vanderpool@gmail.com>
2017-06-02 10:22:14 -0500

    require login for most views

--- ladder/urls.py
+++ ladder/urls.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^submit/', views.MatchFormView.as_view(), name='submit'),
-    url(r'^match/$', views.MatchListView.as_view(), name='match-list'),
-    url(r'^match/unconfirmed$', views.UnconfirmedMatchesView.as_view(), name='unconfirmed-matches'),
-    url(r'^match/(?P<pk>\d+)/$', views.MatchDetailView.as_view(), name='match-detail'),
-    url(r'^match/(?P<pk>\d+)/confirm$', views.confirm_match, name='match-confirm'),
-    url(r'^match/(?P<pk>\d+)/report$', views.MatchReportView.as_view(), name='match-report'),
+    url(r'^submit/', login_required(views.MatchFormView.as_view()), name='submit'),
+    url(r'^match/$', login_required(views.MatchListView.as_view()), name='match-list'),
+    url(r'^match/unconfirmed$', login_required(views.UnconfirmedMatchesView.as_view()), name='unconfirmed-matches'),
+    url(r'^match/(?P<pk>\d+)/$', login_required(views.MatchDetailView.as_view()), name='match-detail'),
+    url(r'^match/(?P<pk>\d+)/confirm$', login_required(views.confirm_match), name='match-confirm'),
+    url(r'^match/(?P<pk>\d+)/report$', login_required(views.MatchReportView.as_view()), name='match-report'),
     url(r'^players$', views.PlayerListView.as_view(), name='player-list'),
 ]
